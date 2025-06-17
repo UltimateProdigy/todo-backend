36ffb94712c650fbf46da91a44cdef77ad3d944c
NSEMOH1 <ayobamiakinola84@gmail.com>
2025-06-17 20:03:36 +0100

    fix

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,21 +1,35 @@
 const Task = require("../models/Task");
 
-const getUserTasks = (req, res) => {
+const getUserTasks = async (req, res) => {
     const { id } = req.params;
-    const tasks = Task.find({ userId: id });
-    if (!tasks) return res.sendStatus(404);
-    res.json({
-        data: tasks,
-    });
+
+    try {
+        const tasks = await Task.find({ userId: id });
+        if (!tasks) return res.sendStatus(404);
+        return res.status(200).json({
+            data: tasks,
+        });
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
 };
 
-const getTask = () => {
+const getTask = async () => {
     const { id } = req.params;
-    const task = Task.findOne({ id: id });
-    if (!task) return res.sendStatus(404);
-    res.status(201).json({
-        data: task,
-    });
+
+    try {
+        const task = await Task.findOne({ id: id });
+        if (!task) return res.status(404).json({ message: "No task found" });
+        return res.status(201).json({
+            data: task,
+        });
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
 };
+
+const createTask = async () => {
+    
+}
 
 module.exports = { getUserTasks, getTask };
